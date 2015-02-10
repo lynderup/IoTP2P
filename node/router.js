c2651c34ad0373b1396c4b3dcdf6f5880c8ad42e
Rohde Fischer <rohdef@rohdef.dk>
2015-02-10 15:53:47 +0100

    Sketches for server

--- node/router.js
+++ node/router.js
@@ -1,8 +1,18 @@
-var route = function(pathname) {
-    console.log("Routing for: " + pathname);
+var route = function(pathname, post, get) {
+  console.log("Routing for: " + pathname);
 
-    // TODO: Here needs ze work be done
-    // should probably recieve the url part, not path name?
+  // TODO: Here needs ze work be done
+  // should probably recieve the url part, not path name?
+
+  var pathParts = pathname.split('/');
+  if (pathParts.length === 2) {
+    // Yay
+    var service = pathParts[0];
+    var fun = pathParts[1];
+    modules[service][fun].call(service, post, get);
+  } else {
+    // Error?
+  }
 };
 
 exports.route = route;
--- node/server.js
+++ node/server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var qs = require('querystring')
 
 var start = function(route) {
     var onRequest = function(request, response) {
@@ -11,6 +12,19 @@ var start = function(route) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello world");
 	response.end();
+
+      if (request.method = "POST") {
+        var body = '';
+        request.on('data', function(data) {
+          body += data;
+
+          if (body.length > 1e6) { // Prevent flooding, 1 mb data or more
+            request.connection.destroy;
+          }
+        });
+
+        var POST = qs.parse(body);
+      }
     };
 
     http.createServer(onRequest).listen(8888);
