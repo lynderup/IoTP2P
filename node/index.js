31720dd659f1e3f77e7b14092600a1acc0ffab15
Rohde Fischer <rohdef@rohdef.dk>
2015-02-17 19:59:28 +0100

    Class based chord

--- node/index.js
+++ node/index.js
@@ -6,5 +6,6 @@ var test = require('./test');
 //var handlers['chord'] = chord;
 var handlers = {};
 handlers['test'] = test;
+//handlers['chord'] = new chord.Chord();
 
 server.start(router.route, handlers);
