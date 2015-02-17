3c625ebbf96c4ae44d1f45fe949e5782421ed7b5
Lynderup <lynderupjensen@gmail.com>
2015-02-17 22:09:37 +0100

    Small changes

--- node/index.js
+++ node/index.js
@@ -1,10 +1,10 @@
 var server = require('./server');
 var router = require('./router');
-//var chord = require('./chord');
+var chord = require('./chord2');
 var test = require('./test');
 
-//var handlers['chord'] = chord;
 var handlers = {};
 handlers['test'] = test;
+handlers['chord'] = chord;
 
 server.start(router.route, handlers);
