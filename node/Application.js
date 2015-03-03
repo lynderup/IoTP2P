f6b4e00f03407d18d61970a765951467f96be79c
Lynderup <lynderupjensen@gmail.com>
2015-03-04 00:50:58 +0100

    Unused stuff

---
+++ node/Application.js
@@ -0,0 +1,15 @@
+
+var Application = function(serialized) {
+
+    var data = [];
+
+    this.id = 0;
+
+    if (serialized) {}
+
+    this.serialize = function(node) {};
+    
+    
+};
+
+exports.Application = Application
