b644f952bea63fdc358cf86c7958e155dab2c781
Lynderup <lynderupjensen@gmail.com>
2015-03-04 00:51:48 +0100

    test data source

--- node/test.js
+++ node/test.js
@@ -1,8 +1,26 @@
-var test = {test: "test"};
+var http = require('http');
+var url = require("url");
 
-var test2 = [];
-test2[0] = test;
-
-test = {test: "test2"};
-
-console.log(test2);
+http.createServer(function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    if (pathname == "/app") {
+        response.writeHeader(200, {"Content-Type": "application/json"})
+        var data = {
+            key : 500,
+            name : "Test App",
+            content : "testtest",
+            appurl : "http://localhost:8090/data"
+        }
+        response.write(JSON.stringify(data))
+        
+    }
+    else if (pathname == "/app") {
+        response.writeHeader(200, {"Content-Type": "application/json"})
+        var data = {
+            data : "testtest"
+        }
+        response.write(JSON.stringify(data))
+    }
+    response.end();
+}).listen(8090)
