fd2d8429a96917e3394b2dae37c81998f28cb503
Rohde Fischer <rohdef@rohdef.dk>
2015-03-03 21:55:17 +0100

    DHT 11 initial implementation

--- spark/main.ino
+++ spark/main.ino
@@ -1,27 +1,41 @@
 double temperature = 0.0;
 int out = 0.0;
 
+int humidity = 0;
+int temperatureFromDht = 0;
+int ms = millis();
+
 const int RED_LED_PIN = A1;
 const int GREEN_LED_PIN = A0;
 const int BLUE_LED_PIN = A4;
 
 const int TEMPERATURE = A3;
 const int PHOTORESISTOR = A5;
+const int DHT11 = D0;
 
 void setup() {
   Spark.variable("temperature", &temperature, DOUBLE);
   Spark.variable("out", &out, INT);
+  Spark.variable("humidity", &humidity, INT);
+  Spark.variable("temperatureFromDht", &temperatureFromDht, INT);
 
   pinMode(TEMPERATURE, INPUT);
   pinMode(PHOTORESISTOR, INPUT);
+  pinMode(DHT11, OUTPUT);
+  digitalWrite(DHT11, HIGH);
 
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
+  if (millis()-ms > 10000) {
+    int dht = readDht11();
+    if (dht == -1) humidity = -1;
+    ms = millis();
+  }
+
   int temperatureReading = 0;
   double voltage = 0.0;
 
@@ -43,7 +57,7 @@ void loop()
 
   int lightReading = analogRead(PHOTORESISTOR);
   // -200 is to make the changes more aparant, will be multiplicated
-  int greenAdj = (lightReading/16)-200;
+  int greenAdj = (lightReading/16)-230;
   if (greenAdj <= 0) greenAdj = 0;
   greenAdj = greenAdj*6.7;
 
@@ -53,5 +67,65 @@ void loop()
   if (greenAdj >= 255) greenAdj = 255;
   analogWrite(GREEN_LED_PIN, greenAdj);
 
-  delay(250);
+  delay(500);
+}
+
+int readDht11() {
+  uint8_t data[] = {0, 0, 0, 0, 0};
+  noInterrupts();
+
+  // Initial protocol, rules are:
+  // Go to LOW >= 18 ms then HIGH, tells DHT11 we want data
+  // DHT11 responds low, high, low in 80 microsecond intervals
+  // if we don't read that, we return with an error.
+  pinMode(DHT11, OUTPUT);
+  digitalWrite(DHT11, LOW);
+  delay(20);
+  digitalWrite(DHT11, INPUT_PULLUP);
+  if (detect_edge(DHT11, HIGH, 10, 200) == -1)
+    return -1;
+  if (detect_edge(DHT11, LOW, 10, 200) == -1)
+    return -1;
+  if (detect_edge(DHT11, HIGH, 10, 200) == -1)
+    return -1;
+
+  // Getting the data, whoa need to check the protocol here
+  for (uint8_t i=0; i<40; i++) {
+    if (detect_edge(DHT11, LOW, 10, 200))
+      return -1;
+
+    int counter = detect_edge(DHT11, HIGH, 10, 200);
+    if (counter == -1)
+      return -1;
+
+    data[i/8] <<= 1;
+
+    if (counter > 4) {
+      data[i/8] |= 1;
+    }
+  }
+  interrupts();
+
+  // Validate the data, last part is a checksum
+  if (data[4] != ((data[0]+data[1]+data[2]) & 0xFF))
+      return -1;
+
+  humidity = data[0];
+  temperatureFromDht = data[2];
+
+  return 0;
+}
+
+int detect_edge(int pin, int val, int interval, int timeout) {
+  int counter = 0;
+  while (digitalRead(pin) == val && counter < timeout) {
+    delayMicroseconds(interval);
+    ++counter;
+  }
+
+  if (counter > timeout) {
+    return -1;
+  }
+
+  return counter;
 }
