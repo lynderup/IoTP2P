8b57f6c537050781aa40d942b3452bc738f42790
Timsen88 <timsen88@gmail.com>
2015-03-10 20:14:59 +0100

    graphs

--- html/index.js
+++ html/index.js
@@ -69,7 +69,7 @@ function init() {
 	    type : "GET",
 	    ContentType : "application/json"
         }).success(function(data){
-            
+
         }).error(function(data){});
     });
 
@@ -95,7 +95,7 @@ function init() {
                 apps.append($app);
             }
         }).error(function(data){});
-        
+
 
         setTimeout(updateApps, 1000);
     };
@@ -141,4 +141,84 @@ function init() {
         setTimeout(updateFingerTable, 1000);
     };
     updateFingerTable();
+
+    var foos = [
+    {
+      "time": "0", temp : "26.5", light :"20.5"
+    },
+    {
+      "time": "2", temp : "26.8", light :"20.6"
+    },
+    {
+      "time": "3", temp : "22.5", light :"20.4"
+    },
+    {
+      "time": "4", temp : "28.5", light :"20.8"
+    },
+    {
+      "time": "6", temp : "20.5", light :"20.9"
+    }
+    ]
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+    .range([0, width]);
+
+    var y = d3.scale.linear()
+    .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+    var line = d3.svg.line().x(function(d) { return x(d.time); })
+                line.y(function(d) { return y(d.temp); });
+
+    var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+      x.domain(d3.extent(foos, function(d) {
+        return d.time; }));
+
+      y.domain(d3.extent(foos, function(d) {
+         return d.temp; }));
+
+      svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .append("text")
+      .attr("class","xtext")
+      .attr("x",width+20)
+      .attr("y",-20)
+      .attr("text-anchor","end")
+      .text("Time");
+
+      svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Temprature");
+
+      svg.append("path")
+      .datum(foos)
+      .attr("class", "line")
+      .attr("d", line);
+
 }
