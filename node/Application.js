089e56eabd1f12e4928bf819d5719f890403f083
Timothi Hansen <timsen88@gmail.com>
2015-03-11 10:41:33 +0100

    2

--- node/Application.js
+++ node/Application.js
@@ -3,79 +3,79 @@ var https = require('http');
 
 var Application = function(node) {
 
-    var localNode = node;
-    var dataSources = [];
-    var updateDataTimer = null;
-    var thisApp = this;
+  var localNode = node;
+  var dataSources = [];
+  var updateDataTimer = null;
+  var thisApp = this;
 
-    this.addSource = function(source) {
-        source.data = [];
-        dataSources.push(source);
-    };
+  this.addSource = function(source) {
+    source.data = [];
+    dataSources.push(source);
+  };
 
 
-    var updateData = function() {
-        var visit = function(i) {
-            if(i == dataSources.length) {
-            } else {
-                var dataSource = dataSources[i];
-                if (dataSource) {
-                    https.get(dataSource.contentUrl, function(res) {
-                        var recievedData = "";
-                        res.on('data', function (chunk) {
-                            recievedData += chunk;
-                        });
-                        res.on('end', function() {
-                            var data = JSON.parse(recievedData).result;
-                            data.time = Date.now();
-                            dataSource.data.push(data);
-                            visit(i+1);
-                        });
-                    }).on('error', function(e) {});
-                }
-            }
-        }
-        visit(0);
-        updateDataTimer = setTimeout(updateData, 1000);
-    };
-    updateData();
-    
-    this.close = function() {
-        clearTimeout(updateDataTimer);
-    };
-
-    this.get_apps = function(callback) {
-        callback(dataSources);
-    };
-
-    this.register_source = function(url) {
-        https.get(url, function(res) {
-           var recievedData = "";
-            res.on('data', function (chunk) {
-                recievedData += chunk;
+  var updateData = function() {
+    var visit = function(i) {
+      if (i == dataSources.length) {} else {
+        var dataSource = dataSources[i];
+        if (dataSource) {
+          https.get(dataSource.contentUrl, function(res) {
+            var recievedData = "";
+            res.on('data', function(chunk) {
+              recievedData += chunk;
             });
             res.on('end', function() {
-                var data = JSON.parse(JSON.parse(recievedData).result);
-                var key = parseInt(data.key)
-                if(key) {
-                    if (localNode.in_interval(key, localNode.predecessor.key, localNode.key)) {
-                        thisApp.addSource(data);
-                    } else {
-                        localNode.find_successor(key, function(node, err) {
-                            if (node) {
-                                node.addSource(data);
-                            }
-                        });
-                    }
-                } else {
-                    //logger.log("No app found on: " + url);
-                }
+              var data = JSON.parse(recievedData).result;
+              data.time = Date.now();
+              dataSource.data.push(data);
+              visit(i + 1);
             });
-        }).on('error', function(e) {
-            //logger.log("App not found");
-        });
-    };
-    
+          }).on('error', function(e) {});
+        }
+      }
+    }
+    visit(0);
+    updateDataTimer = setTimeout(updateData, 1000);
+  };
+  updateData();
+
+  this.close = function() {
+    clearTimeout(updateDataTimer);
+  };
+
+  this.get_apps = function(callback) {
+    callback(dataSources);
+  };
+
+  this.register_source = function(url) {
+    https.get(url, function(res) {
+      var recievedData = "";
+      res.on('data', function(chunk) {
+        recievedData += chunk;
+      });
+      res.on('end', function() {
+        var data = JSON.parse(JSON.parse(recievedData).result);
+        var key = parseInt(data.key)
+        if (key) {
+          if (localNode.in_interval(key, localNode.predecessor.key,
+              localNode.key)) {
+            thisApp.addSource(data);
+          } else {
+            localNode.find_successor(key, function(node, err) {
+              if (node) {
+                node.addSource(data);
+              }
+            });
+          }
+        } else {
+          //logger.log("No app found on: " + url);
+        }
+      });
+    }).on('error', function(e) {
+      //logger.log("App not found");
+    });
+  };
+
 };
 
 exports.Application = Application
